ced6d8972a4f80e7912514dca5f08bc67edef7e9
David LeBauer <dlebauer@email.arizona.edu>
2024-07-09 13:08:42 -0400

    Create backup_zotero.sh

---
+++ bin/backup_zotero.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# sync to ~/backups
+#   this is a symbolic link to my cloud drive e.g.
+#   ln -s "/Users/dlebauer/OneDrive - The LeBauer Approach LLC/backups/" ~/backups
+
+# schedule: Monday at 9AM local time 
+#   crontab -e
+#   # Add a line to schedule the backup. For example, to run the script daily at 2 AM:
+#   0 9 * * 1 ~/backup_zotero.sh
+
+rsync -av --delete /Users/username/Zotero/ /Users/username/backups/zotero_backup/
+
